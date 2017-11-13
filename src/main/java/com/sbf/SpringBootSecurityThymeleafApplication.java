377417061531cf42b86ba741860cd3612b37c9b1
unknown <narak.hok@Recervesmb.sambatfinance.com>
2017-11-13 15:45:23 +0700

    New Project ...!

---
+++ src/main/java/com/sbf/SpringBootSecurityThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.sbf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityThymeleafApplication.class, args);
+	}
+}
